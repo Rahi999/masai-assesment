06079a917438702649cef831f3f5f67681cd4e2f
Rahim Ansari <101566073+Rahi999@users.noreply.github.com>
2022-10-15 06:12:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import Navbar from "./Components/Navbar";
+import HomePage from "./Components/HomePage";
+import { AllRoutes } from "./Components/AllRoutes";
+function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          overflow: "auto",
+          height: "500px",
+          marginTop: "120px"
+        }}
+      >
+        <AllRoutes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
